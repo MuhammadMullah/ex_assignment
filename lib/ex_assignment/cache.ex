bff6122007cf570e427bdcb043bee10419f66b9a
MuhammadMullah <muhammadmullah16@gmail.com>
2023-03-31 22:57:41 +0300

    respective genserver callbacks to handle caching with ets

--- lib/ex_assignment/cache.ex
+++ lib/ex_assignment/cache.ex
@@ -1,7 +1,7 @@
 defmodule ExAssignment.Cache do
   use GenServer
 
-  @timer :timer.minutes(1)
+  @timer :timer.minutes(60)
 
 
   def start_link(_) do
@@ -9,17 +9,42 @@ defmodule ExAssignment.Cache do
   end
 
   def init(_) do
+    :ets.new(:todos, [:set, :public, :named_table])
     scheduler()
     {:ok, %{}}
   end
 
-  def handle_info(:clear_cache, state) do
-    IO.puts("CACHE CLEARED")
-    IO.puts("CACHE CLEARED")
-    IO.puts("CACHE CLEARED")
-    IO.puts("*************")
+  def delete(key) do
+    GenServer.cast(__MODULE__, {:delete, key})
+  end
 
+  def put(key, data) do
+    GenServer.cast(__MODULE__, {:put, key, data})
+  end
+
+
+   def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  def handle_info(:clear_cache, state) do
     scheduler()
+    {:noreply, state}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    t = :ets.lookup(:todos, key)
+
+    {:reply, t, state}
+  end
+
+  def handle_cast({:delete, key}, state) do
+    :ets.delete(:todos, key)
+    {:noreply, state}
+  end
+
+  def handle_cast({:put, key, data}, state) do
+    :ets.insert(:todos, {key, data})
     {:noreply, state}
   end
 
